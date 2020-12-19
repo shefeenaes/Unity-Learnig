2f289e5198065711f8c74acf5721f80fd01ef268
shefeenaes <70755506+shefeenaes@users.noreply.github.com>
2020-12-19 13:14:58 +0530

    car and plane game

---
+++ prototype1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+    public float propSpeed = 1000;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotate the Propeller of the plane
+        
+        transform.Rotate(Vector3.forward * propSpeed * Time.deltaTime);
+
+    }
+}
